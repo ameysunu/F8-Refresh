20f8450576f46a97adf3e66e9ed6cf44a91e7dd3
ameysunu <ameysunu00719@gmail.com>
2021-05-04 11:12:28 +0300

    initial commit

---
+++ android/app/src/main/kotlin/com/example/f8refresh/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.f8refresh
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,6 @@
+import 'package:f8refresh/login.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(debugShowCheckedModeBanner: false, home: Login()));
+}
