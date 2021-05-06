91d6df4fd5bc7482d6345963f6b5b2cc9d47b6f9
ameysunu <ameysunu00719@gmail.com>
2021-05-06 13:49:19 +0300

    ui changes

--- lib/home.dart
+++ lib/home.dart
@@ -52,126 +52,137 @@ class _HomeState extends State<Home> {
       body: SingleChildScrollView(
         child: Padding(
           padding: const EdgeInsets.all(10.0),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Container(
-                height: MediaQuery.of(context).size.height * 0.025,
-              ),
-              Text(
-                "Welcome,",
-                style: TextStyle(fontFamily: 'Gotham', fontSize: 17),
-              ),
-              Container(
-                height: MediaQuery.of(context).size.height * 0.04,
-              ),
-              Text(
-                "${widget.name}",
-                style: TextStyle(fontFamily: 'Gotham', fontSize: 25),
-              ),
-              Container(
-                height: MediaQuery.of(context).size.height * 0.05,
-              ),
-              Container(
-                width: MediaQuery.of(context).size.width * 0.95,
-                child: Container(
-                  decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(15.0),
-                      color: HexColor('#FF6F94')),
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Row(
-                        children: [],
-                      ),
-                      Padding(
-                        padding: const EdgeInsets.fromLTRB(10, 25, 10, 25),
-                        child: Text(
-                          "All our dreams can come true, if we have the courage to pursue them. – Walt Disney.",
-                          style: GoogleFonts.catamaran(
-                              textStyle:
-                                  TextStyle(color: Colors.white, fontSize: 17)),
+          child: Container(
+            decoration: BoxDecoration(
+                image: DecorationImage(
+              image: AssetImage('images/home.png'),
+              fit: BoxFit.cover,
+              colorFilter: ColorFilter.mode(
+                  HexColor('#E5D6FF').withOpacity(0.7), BlendMode.dstATop),
+            )),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Container(
+                  height: MediaQuery.of(context).size.height * 0.025,
+                ),
+                Text(
+                  "Welcome,",
+                  style: TextStyle(fontFamily: 'Gotham', fontSize: 17),
+                ),
+                Container(
+                  height: MediaQuery.of(context).size.height * 0.04,
+                ),
+                Text(
+                  "${widget.name}",
+                  style: TextStyle(fontFamily: 'Gotham', fontSize: 25),
+                ),
+                Container(
+                  height: MediaQuery.of(context).size.height * 0.05,
+                ),
+                Container(
+                  width: MediaQuery.of(context).size.width * 0.95,
+                  child: Container(
+                    decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(15.0),
+                        color: HexColor('#FF6F94')),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Row(
+                          children: [],
                         ),
-                      ),
-                    ],
+                        Padding(
+                          padding: const EdgeInsets.fromLTRB(10, 25, 10, 25),
+                          child: Text(
+                            "All our dreams can come true, if we have the courage to pursue them. – Walt Disney.",
+                            style: GoogleFonts.catamaran(
+                                textStyle: TextStyle(
+                                    color: Colors.white, fontSize: 17)),
+                          ),
+                        ),
+                      ],
+                    ),
                   ),
                 ),
-              ),
-              Container(
-                height: MediaQuery.of(context).size.height * 0.075,
-              ),
-              Text(
-                "Mood Blogger",
-                style: TextStyle(
-                    fontFamily: 'Gotham',
-                    color: HexColor('#3A2F50'),
-                    fontSize: 20),
-              ),
-              Container(
-                height: MediaQuery.of(context).size.height * 0.025,
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  InkWell(
-                    onTap: () {
-                      Navigator.push(context,
-                          MaterialPageRoute(builder: (context) {
-                        return Blog();
-                      }));
-                    },
-                    child: Container(
-                      decoration: BoxDecoration(
-                          borderRadius: BorderRadius.circular(15),
-                          color: HexColor('#FFC8DB')),
-                      child: Column(
-                        children: [
-                          Container(
-                              height: MediaQuery.of(context).size.height * 0.2,
-                              child: Image.asset('images/mood.png')),
-                          Padding(
-                            padding: const EdgeInsets.only(bottom: 10.0),
-                            child: Text(
-                              "Add a mood",
-                              style: GoogleFonts.catamaran(
-                                  textStyle:
-                                      TextStyle(color: HexColor('#F66C9C'))),
-                            ),
-                          )
-                        ],
-                      ),
-                    ),
-                  ),
-                  InkWell(
-                    onTap: () {
-                      Navigator.push(context,
-                          MaterialPageRoute(builder: (context) {
-                        return ViewBlog();
-                      }));
-                    },
-                    child: Container(
-                      decoration: BoxDecoration(
-                          borderRadius: BorderRadius.circular(15),
-                          color: HexColor('#FFCDAE')),
-                      child: Column(
-                        children: [
-                          Container(
-                              height: MediaQuery.of(context).size.height * 0.2,
-                              child: Image.asset('images/moodnew.png')),
-                          Padding(
-                            padding: const EdgeInsets.only(bottom: 10.0),
-                            child: Text("View moods",
+                Container(
+                  height: MediaQuery.of(context).size.height * 0.075,
+                ),
+                Text(
+                  "Mood Blogger",
+                  style: TextStyle(
+                      fontFamily: 'Gotham',
+                      color: HexColor('#3A2F50'),
+                      fontSize: 20),
+                ),
+                Container(
+                  height: MediaQuery.of(context).size.height * 0.025,
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    InkWell(
+                      onTap: () {
+                        Navigator.push(context,
+                            MaterialPageRoute(builder: (context) {
+                          return Blog();
+                        }));
+                      },
+                      child: Container(
+                        decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(15),
+                            color: HexColor('#FFC8DB')),
+                        child: Column(
+                          children: [
+                            Container(
+                                height:
+                                    MediaQuery.of(context).size.height * 0.2,
+                                child: Image.asset('images/mood.png')),
+                            Padding(
+                              padding: const EdgeInsets.only(bottom: 10.0),
+                              child: Text(
+                                "Add a mood",
                                 style: GoogleFonts.catamaran(
                                     textStyle:
-                                        TextStyle(color: HexColor('#EF8D50')))),
-                          )
-                        ],
+                                        TextStyle(color: HexColor('#F66C9C'))),
+                              ),
+                            )
+                          ],
+                        ),
                       ),
                     ),
-                  ),
-                ],
-              )
-            ],
+                    InkWell(
+                      onTap: () {
+                        Navigator.push(context,
+                            MaterialPageRoute(builder: (context) {
+                          return ViewBlog();
+                        }));
+                      },
+                      child: Container(
+                        decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(15),
+                            color: HexColor('#FFCDAE')),
+                        child: Column(
+                          children: [
+                            Container(
+                                height:
+                                    MediaQuery.of(context).size.height * 0.2,
+                                child: Image.asset('images/moodnew.png')),
+                            Padding(
+                              padding: const EdgeInsets.only(bottom: 10.0),
+                              child: Text("View moods",
+                                  style: GoogleFonts.catamaran(
+                                      textStyle: TextStyle(
+                                          color: HexColor('#EF8D50')))),
+                            )
+                          ],
+                        ),
+                      ),
+                    ),
+                  ],
+                )
+              ],
+            ),
           ),
         ),
       ),
