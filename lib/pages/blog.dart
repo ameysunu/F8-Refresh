b70a5bcdb55a721a36be2e85e3e65bf0ddef622d
ameysunu <ameysunu00719@gmail.com>
2021-05-06 12:09:26 +0300

    streambuilder added

--- lib/pages/blog.dart
+++ lib/pages/blog.dart
@@ -2,12 +2,15 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_slidable/flutter_slidable.dart';
 import 'package:hexcolor/hexcolor.dart';
 import 'package:intl/intl.dart';
 
 final titleController = TextEditingController();
 final bodyController = TextEditingController();
 final firestoreInstance = FirebaseFirestore.instance;
+AsyncSnapshot<DocumentSnapshot> snapshot;
+Stream<QuerySnapshot> newStream;
 
 DateTime now = DateTime.now();
 String newDate = DateFormat('EEE d MMM, kk:mm').format(now);
@@ -177,6 +180,17 @@ class ViewBlog extends StatefulWidget {
 
 class _ViewBlogState extends State<ViewBlog> {
   @override
+  void initState() {
+    super.initState();
+    Firebase.initializeApp().whenComplete(() {
+      print("completed");
+      setState(() {
+        newStream = firestoreInstance.collection('mood').snapshots();
+      });
+    });
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: HexColor('#FFCDAE'),
@@ -219,6 +233,129 @@ class _ViewBlogState extends State<ViewBlog> {
                     fontSize: 20,
                     color: HexColor('#FF7018'))),
           ),
+          StreamBuilder(
+              stream: newStream,
+              builder: (BuildContext context, AsyncSnapshot snapshot) {
+                var totalgroupCount = 0;
+                List<DocumentSnapshot> groupUsers;
+                if (snapshot.hasData) {
+                  groupUsers = snapshot.data.documents;
+                  totalgroupCount = groupUsers.length;
+                  return (Container(
+                    child: ListView.builder(
+                        itemCount: groupUsers.length,
+                        itemBuilder: (context, int index) {
+                          return SingleChildScrollView(
+                            child: Padding(
+                              padding: const EdgeInsets.all(10.0),
+                              child: Slidable(
+                                actionPane: SlidableDrawerActionPane(),
+                                actionExtentRatio: 0.25,
+                                child: Container(
+                                  child: Column(
+                                    children: [
+                                      InkWell(
+                                        child: Container(
+                                            decoration: BoxDecoration(
+                                                borderRadius:
+                                                    BorderRadius.circular(15.0),
+                                                gradient: LinearGradient(
+                                                    colors: [
+                                                      HexColor('#FFA583'),
+                                                      HexColor('#EAD74F')
+                                                    ])),
+                                            height: MediaQuery.of(context)
+                                                    .size
+                                                    .height *
+                                                0.2,
+                                            width: MediaQuery.of(context)
+                                                    .size
+                                                    .width *
+                                                1,
+                                            child: Column(
+                                              crossAxisAlignment:
+                                                  CrossAxisAlignment.start,
+                                              children: [
+                                                Padding(
+                                                  padding: const EdgeInsets.all(
+                                                      10.0),
+                                                  child: Icon(
+                                                    Icons.book,
+                                                    color: HexColor('#7D7D7D'),
+                                                  ),
+                                                ),
+                                                Padding(
+                                                  padding: const EdgeInsets.all(
+                                                      10.0),
+                                                  child: Text(
+                                                      groupUsers[index]
+                                                          .data()['time']
+                                                          .toString()
+                                                          .toUpperCase(),
+                                                      style: TextStyle(
+                                                          fontFamily: 'Gotham',
+                                                          fontSize: 16,
+                                                          color: Colors.white)),
+                                                ),
+                                                Padding(
+                                                  padding: const EdgeInsets.all(
+                                                      10.0),
+                                                  child: Text(
+                                                      groupUsers[index]
+                                                          .data()['title'],
+                                                      style: TextStyle(
+                                                          fontFamily: 'Gotham',
+                                                          fontSize: 20,
+                                                          color: HexColor(
+                                                              '#A8617A'))),
+                                                ),
+                                              ],
+                                            )),
+                                        onTap: () {
+                                          print("tapped");
+                                        },
+                                      ),
+                                    ],
+                                  ),
+                                ),
+                                secondaryActions: <Widget>[
+                                  Padding(
+                                    padding: const EdgeInsets.all(10.0),
+                                    child: Container(
+                                      decoration: BoxDecoration(
+                                        borderRadius:
+                                            BorderRadius.circular(15.0),
+                                        color: HexColor('#FF0D0D'),
+                                      ),
+                                      child: Container(
+                                        child: IconButton(
+                                          icon: Icon(Icons.delete,
+                                              color: Colors.white),
+                                          onPressed: () {
+                                            print("Delete triggered");
+                                            firestoreInstance
+                                                .collection('mood')
+                                                .doc(groupUsers[index]
+                                                    .data()['id'])
+                                                .delete()
+                                                .then((_) {
+                                              print("success!");
+                                            });
+                                          },
+                                        ),
+                                      ),
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            ),
+                          );
+                        }),
+                  ));
+                } else {
+                  return Center(child: CircularProgressIndicator());
+                }
+              })
         ],
       ),
     );
