1cfc05a837813ce0732d96ab45db44297dff50b4
ameysunu <ameysunu00719@gmail.com>
2021-05-10 11:51:46 +0300

    user ui done

--- lib/pages/user.dart
+++ lib/pages/user.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:hexcolor/hexcolor.dart';
 
@@ -21,25 +22,183 @@ class _UserState extends State<User> {
           style: TextStyle(fontFamily: 'Gotham', color: Colors.black),
         ),
       ),
-      body: Column(
-        children: [
-          Padding(
-            padding: const EdgeInsets.all(10.0),
-            child: Row(
-              children: [
-                CircleAvatar(
-                  radius: 35,
-                  backgroundColor: HexColor('#E5D6FF'),
-                  backgroundImage: NetworkImage(widget.image),
+      body: Stack(
+        alignment: Alignment.center,
+        children: <Widget>[
+          Positioned(
+            top: 200,
+            child: Container(
+              color: HexColor('#FFFFFF'),
+              width: MediaQuery.of(context).size.width,
+              child: Padding(
+                padding: const EdgeInsets.all(10.0),
+                child: Column(
+                  children: [
+                    Container(
+                      height: MediaQuery.of(context).size.height * 0.05,
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.all(10.0),
+                      child: Row(
+                        children: [
+                          Padding(
+                            padding: const EdgeInsets.only(bottom: 10.0),
+                            child: Icon(
+                              CupertinoIcons.settings,
+                              color: HexColor('#845EC2'),
+                            ),
+                          ),
+                          Padding(
+                            padding: const EdgeInsets.only(left: 10.0),
+                            child: Text(
+                              "Reset all statistics",
+                              style: TextStyle(fontFamily: 'Gotham'),
+                            ),
+                          )
+                        ],
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.all(10.0),
+                      child: Row(
+                        children: [
+                          Padding(
+                            padding: const EdgeInsets.only(bottom: 10.0),
+                            child: Icon(
+                              CupertinoIcons.info_circle,
+                              color: HexColor('#845EC2'),
+                            ),
+                          ),
+                          Padding(
+                            padding: const EdgeInsets.only(left: 10.0),
+                            child: Text(
+                              "Facebook SDK Terms and Conditions",
+                              style: TextStyle(fontFamily: 'Gotham'),
+                            ),
+                          )
+                        ],
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.all(10.0),
+                      child: Row(
+                        children: [
+                          Padding(
+                            padding: const EdgeInsets.only(bottom: 10.0),
+                            child: Icon(
+                              CupertinoIcons.hammer_fill,
+                              color: HexColor('#845EC2'),
+                            ),
+                          ),
+                          Padding(
+                            padding: const EdgeInsets.only(left: 10.0),
+                            child: Text(
+                              "Report an issue",
+                              style: TextStyle(fontFamily: 'Gotham'),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.all(10.0),
+                      child: Row(
+                        children: [
+                          Padding(
+                            padding: const EdgeInsets.only(bottom: 10.0),
+                            child: Icon(
+                              CupertinoIcons.rocket_fill,
+                              color: HexColor('#845EC2'),
+                            ),
+                          ),
+                          Padding(
+                            padding: const EdgeInsets.only(left: 10.0),
+                            child: Text(
+                              "About Us",
+                              style: TextStyle(fontFamily: 'Gotham'),
+                            ),
+                          )
+                        ],
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.all(10.0),
+                      child: Row(
+                        children: [
+                          Padding(
+                            padding: const EdgeInsets.only(bottom: 10.0),
+                            child: Icon(
+                              CupertinoIcons.power,
+                              color: HexColor('#845EC2'),
+                            ),
+                          ),
+                          Padding(
+                            padding: const EdgeInsets.only(left: 10.0),
+                            child: Text(
+                              "Logout",
+                              style: TextStyle(fontFamily: 'Gotham'),
+                            ),
+                          )
+                        ],
+                      ),
+                    ),
+                  ],
                 ),
-                Padding(
-                  padding: const EdgeInsets.only(left: 15.0),
-                  child: Text(
-                    "${widget.name}",
-                    style: TextStyle(fontFamily: 'Gotham', fontSize: 17),
-                  ),
-                )
-              ],
+              ),
+            ),
+          ),
+          Positioned(
+            top: MediaQuery.of(context).size.height * 0.07,
+            left: 15,
+            right: 15,
+            child: Card(
+              elevation: 8,
+              color: Colors.white,
+              shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(10)),
+              child: Container(
+                width: MediaQuery.of(context).size.height * .90,
+                height: MediaQuery.of(context).size.height * 0.2,
+                child: Column(
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.fromLTRB(30.0, 10, 30, 10),
+                      child: Container(
+                        decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(15),
+                            color: Colors.white),
+                        child: Padding(
+                          padding: const EdgeInsets.all(10.0),
+                          child: Column(
+                            children: [
+                              Row(
+                                children: [],
+                              ),
+                              Padding(
+                                padding: const EdgeInsets.all(10.0),
+                                child: CircleAvatar(
+                                  radius: 35,
+                                  backgroundColor: Colors.white,
+                                  backgroundImage: NetworkImage(widget.image),
+                                ),
+                              ),
+                              Padding(
+                                padding: const EdgeInsets.only(
+                                    left: 15.0, top: 15, bottom: 15),
+                                child: Text(
+                                  "${widget.name}",
+                                  style: TextStyle(
+                                      fontFamily: 'Gotham', fontSize: 17),
+                                ),
+                              )
+                            ],
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
             ),
           ),
         ],
