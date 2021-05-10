5cf567e3bbcd604921218475c685d148bcb6cb9a
ameysunu <ameysunu00719@gmail.com>
2021-05-10 14:04:15 +0300

    splash screen added

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:f8refresh/login.dart';
+import 'package:f8refresh/splash.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MaterialApp(debugShowCheckedModeBanner: false, home: Login()));
+  runApp(MaterialApp(debugShowCheckedModeBanner: false, home: SplashScreen()));
 }
